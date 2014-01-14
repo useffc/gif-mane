4b77e7c0a29d0f0d7e3cb9e62d81dfdb323df885
useff <useffc@gmail.com>
2014-01-14 15:57:23 -0500

    takes screens with basic functionality. args hardcoded

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+//declare modules.
 var express = require("express");
-var routes = require('./routes');
+var fs = require('fs');
 var http = require('http');
+var ffmpeg = require('fluent-ffmpeg');
 
+var movie = process.argv[2];
+var duration = process.argv[3];
+var startTime = process.argv[4];
+
+/*var proc = new ffmpeg({
+  source: movie })
+  .withSize(99%)
+  .takeScreenshots({
+    count: duration,
+    timemarks: [startTime, duration]},
+    'shots',
+    function(err, filenames) {
+      console.log('screenshots saved');
+      console.log(filenames);
+     });
+*/
+
+var proc = new ffmpeg({
+  source: movie})
+  .withSize('99%')
+  .takeScreenshots(5, 'shots', function(err, filenames) {
+    if (err) throw err;
+  console.log(filenames);
+  console.log('saved');
+  } );
--- readme.md
+++ readme.md
@@ -5,6 +5,8 @@ gif mane is a node app for making animated gifs from video files.
 
 it uses node, ffmpeg, imagemagick, and some other stuff that i'll detail later.
 
+- pngquant on each file for optimization, THEN convert.
+
 it's cool.
 
 
