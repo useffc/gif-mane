3f291c9fd0a2c9c5ac41a393a44137568e2591aa
useff <useffc@gmail.com>
2014-01-18 18:12:40 -0500

    app now creates frame-by-frame animated gifs

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express = require("express");
 var fs = require('fs');
 var http = require('http');
 var ffmpeg = require('fluent-ffmpeg');
+var imagemagick = require('imagemagick');
 
 //define movie variables & argument vectors.
 var movie = process.argv[2];
 var duration = process.argv[3];
 
 //frame rate is hardcoded right now to 24 fps. In the future, framerate will be derived from ffmpeg metadata.
-var numFrames = duration * 24;
+var FRAMERATE = 24;
+var numFrames = duration * FRAMERATE;
 //startTime will be entered in seconds. In the future, implement way to translate traditionally formatted start time to seconds.
 var startTime = process.argv[4];
 var frameTimes = [];
@@ -17,7 +19,7 @@ var frameTimes = [];
 var newTime = parseFloat(startTime);
 
 for (var i = 0; i < numFrames; i = i + 1) {
-  newTime = newTime + 0.416;
+  newTime = newTime + (1/FRAMERATE);
 
   frameTimes[i] = newTime.toString();
 }
@@ -25,6 +27,9 @@ for (var i = 0; i < numFrames; i = i + 1) {
 var ffmpegInstance = new ffmpeg({
   source: movie
 }).withSize('99%')
+  .onProgress(function(progress) {
+    console.log(progress.percent + '%');
+  })
   .takeScreenshots({
     count: numFrames,
     timemarks: frameTimes,
@@ -33,4 +38,14 @@ var ffmpegInstance = new ffmpeg({
     if (err) throw err;
     console.log('screenshots saved');
     console.log(filenames);
+
+
+    //imagemagick function. Second argv should be variable, will be between 3 and 7, will translate to fast medium slow.
+    imagemagick.convert(['-delay', '5', '-loop', '0', 'shots/*.jpg', 'gif.gif'],
+      function(err, data){
+        if (err) throw err;
+        console.log(data);
+      }
+    );
   });
+
