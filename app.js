f1c1c9db66513382fd007fc39b43dc047f222560
useff <useffc@gmail.com>
2014-01-18 16:30:42 -0500

    spits out files. the same image every time

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-//declare modules.
+//define modules.
 var express = require("express");
 var fs = require('fs');
 var http = require('http');
 var ffmpeg = require('fluent-ffmpeg');
 
+//define movie variables & argument vectors.
 var movie = process.argv[2];
 var duration = process.argv[3];
-var startTime = process.argv[4];
 
-/*var proc = new ffmpeg({
+//frame rate is hardcoded right now to 24 fps. In the future, framerate will be derived from ffmpeg metadata.
+var numFrames = duration * 24;
+//startTime will be entered in seconds. In the future, implement way to translate traditionally formatted start time to seconds.
+var startTime = process.argv[4];
+var frameTimes = [];
+
+var newTime = startTime;
+
+/*for (var i = 0; i < numFrames; i = i + 1) {
+  newTime = newTime + 0.416;
+}*/
+
+
+for (var i = 0; i < numFrames; i = i + 1) {
+  frameTimes[i] = newTime;
+}
+
+
+var proc = new ffmpeg({
   source: movie })
-  .withSize(99%)
+  .withSize('99%')
   .takeScreenshots({
-    count: duration,
-    timemarks: [startTime, duration]},
+    count: numFrames,
+    timemarks: frameTimes,
+    filename: '%b_shot_%000i'},
     'shots',
     function(err, filenames) {
+      if (err) throw err;
       console.log('screenshots saved');
       console.log(filenames);
      });
-*/
-
-var proc = new ffmpeg({
-  source: movie})
-  .withSize('99%')
-  .takeScreenshots(5, 'shots', function(err, filenames) {
-    if (err) throw err;
-  console.log(filenames);
-  console.log('saved');
-  } );
