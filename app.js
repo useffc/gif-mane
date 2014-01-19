00f78682307ebbe88fbe0f77c2b5d88b7cbca10c
useff <useffc@gmail.com>
2014-01-18 19:40:36 -0500

    give uploader functionality

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 //responsibility: handling user input
 
-var express = ('express');
+var express = require('express');
+var app = express();
 var processor = require('./processor');
 
 
+app.use(express.bodyParser({keepExtensions: true, uploadDir: process.cwd() + '/movies'}));
+
+
+
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.post('/upload', function(request, response) {
+  console.log(request.files.movie);
+  movieToGif(request.files.movie.path, '1', '0');
+  response.send(request.files.movie.path);
+});
+
+app.engine('html', require('hogan-express'));
+
+app.listen(3000);
+console.log('listening on 3000');
+
 //define movie variables & argument vectors.
-var movieFile = process.argv[2];
-var duration = process.argv[3];
 
-//startTime will be entered in seconds. In the future, implement way to translate traditionally formatted start time to seconds.
-var startTime = process.argv[4];
+var movieToGif = function(movieFile, duration, startTime) {
+  movieFile = movieFile || process.argv[2];
+  duration = duration || process.argv[3];
 
-processor(movieFile, duration, startTime);
+  //startTime will be entered in seconds. In the future, implement way to translate traditionally formatted start time to seconds.
+  startTime = startTime || process.argv[4];
+
+  processor(movieFile, duration, startTime);
+};
