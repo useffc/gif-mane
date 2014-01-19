22f75d5a7ccb73356d525858492f97f14ba99a33
useff <useffc@gmail.com>
2014-01-18 21:22:16 -0500

    app gives url

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.get('/', function(request, response) {
 });
 
 app.post('/upload', function(request, response) {
-  console.log(request.files.movie);
-  movieToGif(request.files.movie.path, '1', '0');
-  response.send(request.files.movie.path);
+  movieToGif(request.files.movie.path, '1', '0', function(filename){
+    response.send(filename);
+  });
 });
 
 app.engine('html', require('hogan-express'));
@@ -26,12 +26,12 @@ console.log('listening on 3000');
 
 //define movie variables & argument vectors.
 
-var movieToGif = function(movieFile, duration, startTime) {
+var movieToGif = function(movieFile, duration, startTime, callback) {
   movieFile = movieFile || process.argv[2];
   duration = duration || process.argv[3];
 
   //startTime will be entered in seconds. In the future, implement way to translate traditionally formatted start time to seconds.
   startTime = startTime || process.argv[4];
 
-  processor(movieFile, duration, startTime);
+  processor(movieFile, duration, startTime, callback);
 };
--- processor.js
+++ processor.js
@@ -11,7 +11,7 @@ var frameTimes = [];
 var FRAMERATE = 24;
 
 
-module.exports = function(movieFile, duration, startTime) {
+module.exports = function(movieFile, duration, startTime, callback) {
 
   var numFrames = duration * FRAMERATE;
   var newTime = parseFloat(startTime);
@@ -39,10 +39,13 @@ module.exports = function(movieFile, duration, startTime) {
 
 
       //imagemagick function. Second argv should be variable, will be between 3 and 7, will translate to fast medium slow.
-      imagemagick.convert(['-delay', '5', '-loop', '0', 'shots/*.jpg', 'gif.gif'],
+      var gifFileName = 'gif/' + movieFile.match(/.([^/]*)[.$]/)[1] + '.gif';
+      imagemagick.convert(['-delay', '5', '-loop', '0', 'shots/*.jpg', gifFileName],
         function(err, data){
           if (err) throw err;
           console.log(data);
+          console.log('gif saved');
+          callback(gifFileName);
         }
       );
     });
